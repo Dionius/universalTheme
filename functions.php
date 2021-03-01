8c3cf5f91a496841c50a1e703419a41515f8efa1
Dionius <antonboichak@gmail.com>
2021-03-01 22:18:46 +0200

    add index

--- functions.php
+++ functions.php
@@ -1,7 +1,32 @@
 <?php 
+//Добавление расширенных возможностей
+if ( ! function_exists( 'universal_theme_setup' ) ) :
+  function universal_theme_setup() {
+
+    //Добавление тега title
+    add_theme_support( 'title-tag' );
+
+    //Добавление Лого на сайт 
+    add_theme_support( 'custom-logo', [
+         'width'       => 163,
+         'flex-height' => true,
+         'header-text' => 'Universal',
+         'unlink-homepage-logo' => false, // WP 5.5
+    ] );
+
+    //Регистрация меню
+        register_nav_menus( [
+          'header_menu' => 'Меню в шапке',
+          'footer_menu' => 'Меню в подвале'
+        ] );
+  }
+endif;
+add_action( 'after_setup_theme', 'universal_theme_setup' );
+
 // правильный способ подключить стили и скрипты
-add_action( 'wp_enqueue_scripts', 'enqueue_universal_style' );
+
 function enqueue_universal_style() {
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
-  wp_enqueue_style( 'universal-theme', get_template_directory_uri(  ) . '/assets/css/universal-theme.css', 'style', null, null );
+  wp_enqueue_style( 'universal-theme', get_template_directory_uri(  ) . '/assets/css/universal-theme.css', 'style');
 }
+add_action( 'wp_enqueue_scripts', 'enqueue_universal_style' );
--- header.php
+++ header.php
@@ -1,10 +1,34 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Universal</title>
   <?php wp_head(); ?>
+
 </head>
-<body>
+<body <?php body_class(); ?> >
+<?php wp_body_open(); ?> 
+<header class ="header">
+    <div class="container">
+      <div class="header-wrapper">
+        <?php 
+          if( has_custom_logo() ){
+            // логотип есть выводим его
+            the_custom_logo();
+          }else{
+            echo'Universal';
+          }
+        ?>
+        <?php 
+          wp_nav_menu( [
+              'theme_location'  => 'header_menu',
+              'container'       => 'nav', 
+              'container_class' => 'header-nav', 
+              'menu_class'      => 'header-menu',
+              'echo'            => true,
+          ] ); 
+        ?>
+        <?php echo get_search_form( ); ?>
+      </div>
+    </div>
+</header>
